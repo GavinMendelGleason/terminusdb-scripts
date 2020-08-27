c12670b3a6f2813c19b749eb69442ed8935b7ffc
Gavin Mendel-Gleason <jacobian@gmail.com>
2020-08-27 20:30:16 +0200

    Adding fixes to squash

--- squash.py
+++ squash.py
@@ -10,15 +10,16 @@ from terminusdb_client import WOQLQuery as WQ
 # Assumes database already exists.
 
 # openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt
-server_url = "https://127.0.0.1:6363"
-#server_url = "https://195.201.12.87:6366"
+#server_url = "https://127.0.0.1:6363"
+server_url = "https://195.201.12.87:6366"
 db = "DBpedia_import_rebase_squash_reset"
 db_label = "DBpedia: import/rebase/squash/reset"
 db_comment = "A test of various pipeline operations"
 user = "admin"
 account = "admin"
 key = "root"
-properties_directory = 'properties_xa_200k_test'
+object_properties_directory = 'object_properties_xa_200k_test'
+literal_properties_directory = 'literal_properties_xa_200k_test'
 types_directory = 'instance_xa_200k_test'
 
 if len(sys.argv) > 1 and (sys.argv[1] == '-optimize'
@@ -57,13 +58,14 @@ print(f"# Starting run with optimization == {optimize}{space}              #")
 print("#                                                        #")
 print("##########################################################")
 print("")
+
 # Load the perties branch
-client.branch('properties')
-client.checkout('properties')
+client.branch('literal_properties')
+client.checkout('literal_properties')
 times = []
-print(f"Importing properties from {properties_directory}")
-for f in os.listdir(properties_directory):
-    filename = f'{properties_directory}/{f}'
+print(f"Importing literal properties from {literal_properties_directory}")
+for f in os.listdir(literal_properties_directory):
+    filename = f'{literal_properties_directory}/{f}'
     ttl_file = open(filename)
     contents = ttl_file.read()
     ttl_file.close()
@@ -75,7 +77,7 @@ for f in os.listdir(properties_directory):
     client.insert_triples(
         "instance","main",
         contents,
-        f"Adding properties in 100k chunk ({f})")
+        f"Adding literal properties in 100k chunk ({f})")
     after = time.time()
 
 
@@ -84,6 +86,26 @@ for f in os.listdir(properties_directory):
     print(f"Update took {total} seconds")
 print(times)
 report_total(times)
+
+client.branch('object_properties')
+client.checkout('object_properties')
+times = []
+print(f"Importing object properties from {object_properties_directory}")
+for f in os.listdir(object_properties_directory):
+    filename = f'{object_properties_directory}/{f}'
+    ttl_file = open(filename)
+    contents = ttl_file.read()
+    ttl_file.close()
+
+    # start the chunk work
+    before = time.time()
+    if optimize == True:
+        optimizer(client)
+    client.insert_triples(
+        "instance","main",
+        contents,
+        f"Adding object properties in 100k chunk ({f})")
+    after = time.time()
 
 # Branch from 'main' to 'types'
 client.checkout('main')
