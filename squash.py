0430cc5a1cc528101508682a3837cb03c1e6aaeb
Gavin Mendel-Gleason <jacobian@gmail.com>
2020-08-28 10:14:07 +0200

    Fixing total time printing

--- squash.py
+++ squash.py
@@ -4,8 +4,10 @@ import os
 import sys
 import time
 import math
+
 from terminusdb_client import WOQLClient
 from terminusdb_client import WOQLQuery as WQ
+
 
 # Assumes database already exists.
 
@@ -18,9 +20,9 @@ db_comment = "A test of various pipeline operations"
 user = "admin"
 account = "admin"
 key = "root"
-object_properties_directory = 'object_properties_xa_200k_test'
 literal_properties_directory = 'literal_properties_xa_200k_test'
-types_directory = 'instance_xa_200k_test'
+object_properties_directory = 'object_properties_xa_200k_test'
+types_directory = 'instance_types_xa_200k_test'
 
 if len(sys.argv) > 1 and (sys.argv[1] == '-optimize'
                           or sys.argv[1] == '--optimize'):
@@ -29,8 +31,8 @@ else:
     optimize = False
 
 space = "" if optimize else "  "
-client = WOQLClient(server_url)
-client.connect(user=user, account=account, key=key, db=db)
+client = WOQLClient(server_url, insecure=True)
+client.connect(user=user, account=account, key=key, db=db, insecure=True)
 
 def optimizer(client):
     client.optimize(f'{account}/{db}/_meta')
@@ -40,7 +42,7 @@ def report_total(times):
     total = sum(times)
     hours = math.floor(total / 3600)
     minutes = math.floor((total - hours * 3600) / 60)
-    seconds = math.floor((total - hours * 3600 - minutes * 60) / 60)
+    seconds = math.floor((total - hours * 3600 - minutes * 60))
     print(f"Total time {hours}:{minutes}:{seconds}")
 
 try:
@@ -87,7 +89,7 @@ for f in os.listdir(literal_properties_directory):
 print(times)
 report_total(times)
 
-client.branch('object_properties')
+client.branch('object_properties', empty=True)
 client.checkout('object_properties')
 times = []
 print(f"Importing object properties from {object_properties_directory}")
@@ -107,9 +109,11 @@ for f in os.listdir(object_properties_directory):
         f"Adding object properties in 100k chunk ({f})")
     after = time.time()
 
-# Branch from 'main' to 'types'
-client.checkout('main')
-client.branch('types')
+    total = (after - before)
+    times.append(total)
+    print(f"Update took {total} seconds")
+
+client.branch('types', empty=True)
 client.checkout('types')
 times = []
 print(f"Importing types from {types_directory}")
@@ -136,12 +140,43 @@ for f in os.listdir(types_directory):
 print(times)
 report_total(times)
 
-print("Rebasing")
+print("Rebasing instance properties")
 client.checkout('main')
-client.rebase({"rebase_from": f'{user}/{db}/local/branch/properties',
+before = time.time()
+client.rebase({"rebase_from": f'{user}/{db}/local/branch/literal_properties',
+               "author": user,
+               "message": "Merging instance properties into main"})
+after = time.time()
+total = (after - before)
+print(f"Rebase took {total} seconds")
+report_total([total])
+
+
+print("Rebasing object properties")
+client.checkout('main')
+before = time.time()
+client.rebase({"rebase_from": f'{user}/{db}/local/branch/object_properties',
+               "author": user,
+               "message": "Merging object properties into main"})
+after = time.time()
+total = (after - before)
+print(f"Rebase took {total} seconds")
+report_total([total])
+
+print("Rebasing types")
+client.checkout('main')
+before = time.time()
+after = time.time()
+client.checkout('main')
+client.rebase({"rebase_from": f'{user}/{db}/local/branch/types',
                "author": user,
                "message": "Merging types into main"})
+total = (after - before)
+print(f"Rebase took {total} seconds")
+report_total([total])
 
+
+print(f"Squashing main")
 before = time.time()
 result = client.squash('Squash commit of properties and types')
 after = time.time()
@@ -149,5 +184,6 @@ total = (after - before)
 print(f"Squash took {total} seconds")
 report_total([total])
 
-print("Branch reset")
-client.reset(result['api:commit'])
+commit = result['api:commit']
+print("Branch reset to {commit}")
+client.reset(commit)
